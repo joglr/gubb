0c4d0e4cc203697fd8be85ac12d6c804a0f86d34
joglr <jonas.g.roessum@gmail.com>
2020-12-23 02:24:30 +0100

    Remove dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import boxen from 'boxen';
-import chalk from 'chalk';
-import cardinal from 'cardinal';
-
 const exit = () => process.exit(1);
 
 export function runTests(r, paths) {
@@ -11,22 +7,18 @@ export function runTests(r, paths) {
   });
 }
 
-const borderStyle = {
-  topLeft: chalk.grey('┌'),
-  topRight: chalk.grey('┐'),
-  bottomLeft: chalk.grey('└'),
-  bottomRight: chalk.grey('┘'),
-  horizontal: chalk.grey('─'),
-  vertical: chalk.grey('│'),
-};
+// const borderStyle = {
+//   topLeft: chalk.grey('┌'),
+//   topRight: chalk.grey('┐'),
+//   bottomLeft: chalk.grey('└'),
+//   bottomRight: chalk.grey('┘'),
+//   horizontal: chalk.grey('─'),
+//   vertical: chalk.grey('│'),
+// };
 
 export const test = (name, fn) => {
   global.console.log(
-    boxen(chalk.bold(name), {
-      borderStyle: boxen.BorderStyle.Double,
-      padding: { left: 1, right: 1, bottom: 0, top: 0 },
-    })
-  );
+    name
   fn();
 };
 
@@ -40,16 +32,15 @@ export const assert = (desc, cond, exitFn?) => {
     success = false;
   }
   if (success) {
-    global.console.log(chalk.bold.green('PASS'), desc);
+    global.console.log('PASS', desc);
   } else {
-    global.console.log(chalk.bold.red('FAIL'), desc);
+    global.console.log('FAIL', desc);
     const error = new Error(errorObj);
     const stackLines = error.stack ? error.stack.split('\n') : [];
     const line = stackLines ? stackLines[2] : 'N/A';
     const [, , lineNumber] = line.split(':');
 
     global.console.log(
-      boxen(
         indent(
           reindent(cond.toString()),
           (line, n) =>
@@ -57,18 +48,7 @@ export const assert = (desc, cond, exitFn?) => {
               chalk.blue(Number(lineNumber) + n)
               // + lineNumber
             } ${chalk.grey('│')} ${cardinal.highlight(line)}`
-        ),
-        {
-          padding: {
-            top: 0,
-            left: 0,
-            right: 1,
-            bottom: 0,
-          },
-          borderColor: 'red',
-          borderStyle,
-        }
-      )
+        )
     );
     const [line1, , line2] = stackLines
       ? stackLines.slice(2, stackLines.length - 6)
